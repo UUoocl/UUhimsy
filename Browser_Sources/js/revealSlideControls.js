a59e1682e1f204ae767cc5dd5f472f319cc9ef2f
UUoocl <99063397+UUoocl@users.noreply.github.com>
2024-11-18 23:44:04 -0500

    reveal controls

--- Browser_Sources/js/revealSlideControls.js
+++ Browser_Sources/js/revealSlideControls.js
@@ -1,70 +1,164 @@
+var currentSlideAttributes, currentfragment, currentEvent, teleprompterSpeed;
 //OBS HotKey Controls
-obs.on("SceneItemEnableStateChanged", async function(event) {
-    //if slide control source changed
-    if (event.sceneItemEnabled === true && event.sceneName === "Slide Controls") {
-      //get source name
-      const sceneItem = await obs.call("GetSceneItemSource", {
-        sceneName: "Slide Controls",
-        sceneItemId: event.sceneItemId,
-      })
-   
-      console.log("hi",sceneItem)
-      const control = sceneItem.sourceName;
-   
-      //if next
-      switch (control) {
-        case "NextSlide":
-            Reveal.next();
-            break;
-        case "PreviousSlide":
-            Reveal.prev();
-            break;
-        case "Play-Pause":
-        //  togglePlayPause();
-          break;
-        case "Text Smaller":
-        //  changeTeleprompterSize({ data: { change: -1 } });
-          break;
-        case "Text Bigger":
-        //  changeTeleprompterSize({ data: { change: 1 } });
-          break;
-        case "Scroll Slower":
-        //  changeTeleprompterSpeed({ data: { change: -1 } });
-          break;
-        case "Scroll Faster":
-        //  changeTeleprompterSpeed({ data: { change: 1 } });
-          break;
-      }
-  
-      await obs.call("SetSceneItemEnabled", {
-        sceneName: "Slide Controls",
-        sceneItemId: event.sceneItemId,
-        sceneItemEnabled: false
-      });
-    }
-  });
+obs.on("SceneItemEnableStateChanged", async function (event) {
+  //if slide control source changed
+  if (event.sceneItemEnabled === true && event.sceneName === "Slide Controls") {
+    //get source name
+    const sceneItem = await obs.call("GetSceneItemSource", {
+      sceneName: "Slide Controls",
+      sceneItemId: event.sceneItemId,
+    });
 
-var currentSlideAttributes, currentfragment, currentEvent;
+    console.log("change", sceneItem);
+    const control = sceneItem.sourceName;
+
+    //if next
+    switch (control) {
+      case "NextSlide":
+        Reveal.next();
+        break;
+      case "PreviousSlide":
+        Reveal.prev();
+        break;
+      case "Play-Pause":
+        //  togglePlayPause();
+        break;
+      case "Text Smaller":
+        //  changeTeleprompterSize({ data: { change: -1 } });
+        break;
+      case "Text Bigger":
+        //  changeTeleprompterSize({ data: { change: 1 } });
+        break;
+      case "Scroll Slower":
+        //  changeTeleprompterSpeed({ data: { change: -1 } });
+        break;
+      case "Scroll Faster":
+        //  changeTeleprompterSpeed({ data: { change: 1 } });
+        break;
+    }
+
+    await obs.call("SetSceneItemEnabled", {
+      sceneName: "Slide Controls",
+      sceneItemId: event.sceneItemId,
+      sceneItemEnabled: false,
+    });
+  }
+});
+
+obs.on("InputSettingsChanged", async function(event){
+  let tags = ["SceneTag", "CameraTag"];
+  if(tags.includes(event.inputName)){
+    console.table(event.inputSettings.text)
+    //split tag 
+    tag = event.inputSettings.text.split("|")
+    //let currentSlide = document.querySelector(".present")
+    //add tag to slide attributes
+    let currentSlide = document.getElementsByClassName("present")[0]
+    console.log(currentSlide.dataset);
+    currentSlide.dataset[`${tag[0]}`] = tag[1];
+    console.log(currentSlide.dataset[`${tag[0]}`])
+    console.log(tag[0])
+    console.log(tag[0].startsWith('scene'))
+    //change to selected tag scene 
+    switch(tag[0].slice(0,8)){
+      case ('sceneEnt'):
+        console.log("switch 1 true", tag[1])
+      obs.call('SetCurrentProgramScene', {sceneName: `scene|||${tag[1]}`})
+      break;
+      case ('cameraEn'):
+        console.log("switch 2 true", tag[1])
+        //obs.call("SetCurrentProgramScene", { sceneName: `scene|||${tag[1]}` });
+        let cameraSources = await obs.call("GetSceneItemList", {
+          sceneName: "Camera",
+        });
+        cameraSources.sceneItems.forEach(async (source) => {
+          if (source.sourceName === tag[1]) {
+            await obs.call("SetSceneItemEnabled", {
+              sceneName: "Camera",
+              sceneItemId: source.sceneItemId,
+              sceneItemEnabled: true,
+            });
+          }
+          if (source.sourceName !== tag[1]) {
+            await obs.call("SetSceneItemEnabled", {
+              sceneName: "Camera",
+              sceneItemId: source.sceneItemId,
+              sceneItemEnabled: false,
+            });
+          }
+        });
+      break;
+    }
+  }
+});
 
 //Set Reveal Slide Data Attribute test
 function setCurrentSlideAttribute(event, key, value){
     currentEvent.currentSlide.dataset[key] = value
 }
 
-Reveal.on('slidechanged', (event) => {
+Reveal.on('slidechanged', async (event) => {
+  //get on exit attributes
     console.log(event)
     // const dataAttr = JSON.parse(JSON.stringify(event.previousSlide.dataset));
     currentEvent = event
     currentSlideAttributes = {...event.previousSlide.dataset};
-    setCurrentSlideAttribute(event,"candy","cane")
-    console.log(currentSlideAttributes)
-    
-    for (let [key, value] of Object.entries(currentSlideAttributes)) {
-        if(key){
-            console.log(`Key found ${key}, with value ${value}`);
-        }
+    if(currentSlideAttributes.hasOwnProperty("sceneExit")){
+        console.log("exit found")
+      obs.call('SetCurrentProgramScene', {sceneName: `scene|||${currentSlideAttributes.sceneExit}`})
     }
-})
+    //setCurrentSlideAttribute(event,"candy","cane")
+    //console.log(currentSlideAttributes)
+    
+    //for (let [key, value] of Object.entries(currentSlideAttributes)) {
+      //  if(key){
+       //     console.log(`Key found ${key}, with value ${value}`);
+        //}
+   // }
+    let slideNotes = Reveal.getSlideNotes()
+    console.log(slideNotes)
+    console.log(typeof slideNotes)
+    console.log(slideNotes.length)
+
+    await obs.call("SetInputSettings", {
+      inputName: "Slide Notes Text",
+      inputSettings: {
+        text: slideNotes
+      }
+    });
+
+    //await getTeleprompterSpeed() 
+    
+    //Reset the teleprompter
+    await obs
+    .call("SetSourceFilterSettings", {
+      sourceName: "Slide Notes Text",
+      filterName: "Scroll",
+      filterSettings: {
+        speed_y: 0
+      }
+    })
+    .then(
+      setTimeout(async () => {
+        await obs.call("SetSourceFilterSettings", {
+          sourceName: "Slide Notes Text",
+          filterName: "Scroll",
+          filterSettings: {
+            speed_y: 15 //teleprompterSpeed
+          }
+        });
+      }, 1000)
+    );
+  })
+  
+  async function getTeleprompterSpeed() {
+    teleprompterSpeed = await obs.call("GetSourceFilter", {
+      sourceName: "Slide Notes Text",
+      filterName: "Scroll"
+    });
+    teleprompterSpeed = teleprompterSpeed.filterSettings.speed_y;
+    //document.getElementById("speedValue").value = teleprompterSpeed;
+  }
 
 Reveal.on('fragmentshown', (event) => {
     console.log(event)
@@ -81,7 +175,6 @@ Reveal.on('fragmentshown', (event) => {
 })
 
 //     let slideTitle = Reveal.getCurrentSlide().attributes.id ? Reveal.getCurrentSlide().attributes.id.value : ""
-//     let slideNotes = Reveal.getSlideNotes()
 //     let slideScene = ''
 //     //if notes contains '|' split text. 
 //     if(slideNotes.includes("|")){
